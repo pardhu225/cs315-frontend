0c30f9df2ba0d60c319d49a1b7d8ef5b3d00dc39
pardhu225 <chowdary1969@gmail.com>
2019-03-23 18:06:01 +0530

    nav bar completed

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserStatusService } from 'src/app/services/user-status.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import iziToast from 'izitoast';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(public uss: UserStatusService, private auth: AngularFireAuth) { }
 
   ngOnInit() {
   }
 
+  resendEmail() {
+    this.auth.auth.currentUser.sendEmailVerification().then(r => {
+      iziToast.success({
+        title: 'Success',
+        message: 'Verification email has been sent'
+      });
+    }).catch(e => {
+      console.log(e);
+      iziToast.error({
+        title: 'Error',
+        message: 'Verification email has not been sent due to unknown error'
+      });
+    });
+  }
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserStatusService } from 'src/app/services/user-status.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public uss: UserStatusService) { }
 
   ngOnInit() {
   }
