0ea5a62f2d9a5eb3a35f2c1527d83b842acc43bf
pardhu225 <chowdary1969@gmail.com>
2019-03-23 11:21:49 +0530

    migrating to ng7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: 'AIzaSyBfaJblOpV87LYEtzDTpCFZdwH6xgysseE',
+  authDomain: 'cs315-project.firebaseapp.com',
+  databaseURL: 'https://cs315-project.firebaseio.com',
+  projectId: 'cs315-project',
+  storageBucket: 'cs315-project.appspot.com',
+  messagingSenderId: '489044652164'
+};
+
 @NgModule({
   declarations: [
     AppComponent
